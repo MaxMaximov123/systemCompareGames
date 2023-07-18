d221df2521fc68420233533d36ff3dfd750600db
Max Maximov <maxss.k2n@yandex.ru>
2023-07-18 21:26:09 +0300

    add sim

--- similarGames/Nodejs/main.js
+++ similarGames/Nodejs/main.js
@@ -191,7 +191,7 @@ async function start(sportKey) {
 
     while (true){
         const game1Ids = await db('games').select('id', 'bookieKey', 'team1Name', 'team2Name', 'isLive', 'globalGameId').where('sportKey', sportKey) // получение списка id1
-
+        
         if (game1Ids){
             for (let game1Id of game1Ids){
                 const timeFrame1 = (await db('outcomes').min('now as startTime').max('now as finTime').where('id', game1Id.id))[0];
@@ -247,7 +247,7 @@ async function start(sportKey) {
             
                                     if (pairExist.length === 0){
                                     try {
-                                        db('pairs').insert({
+                                        await db('pairs').insert({
                                             'id1': game1Id.id,
                                             'id2': game2Id.id,
                                             'isLive': game1Id.isLive,
