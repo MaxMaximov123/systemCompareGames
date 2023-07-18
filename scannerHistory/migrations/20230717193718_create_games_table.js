07797d830f468ff74b5c635e55c95990d102891e
Max Maximov <maxss.k2n@yandex.ru>
2023-07-18 19:36:52 +0300

    add sim

--- scannerHistory/migrations/20230717193718_create_games_table.js
+++ scannerHistory/migrations/20230717193718_create_games_table.js
@@ -2,6 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('games', function(table) {
       table.bigint('id').primary();
       table.bigint('globalGameId');
+      table.boolean('isLive');
       table.string('team1Id');
       table.string('team2Id');
       table.string('team1Name');
