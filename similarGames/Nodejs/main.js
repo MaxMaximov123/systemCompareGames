9bb7afbd1f077101cb5650bef073235284f79e80
Max Maximov <maxss.k2n@yandex.ru>
2023-08-01 19:24:42 +0300

    add graphic

--- similarGames/Nodejs/main.js
+++ similarGames/Nodejs/main.js
@@ -299,8 +299,12 @@ async function start(sportKey) {
                                     (forecastNames['n1+n4'].sameWordsCount + forecastNames['n2+n3'].sameWordsCount) / 2);
 
                                 var needGroup = false;
-                                if (totalNames === 1 && totalOutcomes[0] >= 0.8 && totalScores[0] >= 0.8) needGroup = true;
-                                if (totalNames >= 0.75 && totalOutcomes[0] >= 0.9 && totalScores[0] >= 0.85) needGroup = true;
+                                if (totalNames >= 0.95 && totalOutcomes[0] >= 0.8 && totalScores[0] >= 0.8){
+                                    needGroup = true;
+                                } 
+                                if (totalNames >= 0.75 && totalOutcomes[0] >= 0.9 && totalScores[0] >= 0.85){
+                                    needGroup = true;
+                                } 
                                 console.log('Comparing...', game1Id.id, game2Id.id, totalOutcomes[0], totalScores[0], totalNames, needGroup);
                                 try {
                                     await db('pairs').insert({
@@ -315,7 +319,7 @@ async function start(sportKey) {
                                         'similarityOutcomes': totalOutcomes[0],
                                         'similarityScores': totalScores[0],
                                         'totalSimilarity': (totalOutcomes[0] + totalScores[0]) / 2,
-                                        'needGroup': totalOutcomes[1] && totalScores[1],
+                                        'needGroup': needGroup,
                                         'grouped': game1Id.globalGameId === game2Id.globalGameId,
                                         'now': new Date().getTime(),
                                     });
