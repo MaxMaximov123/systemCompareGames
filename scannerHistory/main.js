cc47ee3d388d45847c76d164ec5c8ff4ba52b80a
Max Maximov <maxss.k2n@yandex.ru>
2023-09-02 20:37:29 +0300

    upd

--- scannerHistory/main.js
+++ scannerHistory/main.js
@@ -253,28 +253,6 @@ socket.on('message', async (message) => {
 					liveTill: new Date(game?.liveTill).getTime(),
 					lastUpdate: new Date().getTime(),
 				});
-				try {
-					await db('startTimeUpdates').insert({
-						gameId: game.id,
-						startTime: new Date(game.startTime),
-						time: new Date()
-					});
-					console.log('update startTime');
-				} catch (error) {console.error(error)}
-				try {
-					const lastNamesUpdate = await db('teamsNamesUpdates')
-					.select('team1Name', 'team2Name')
-					.where('gameId', game.id).orderBy('id', 'desc').limit(1);
-					if (lastNamesUpdate[0].team1Name !== game.team1?.name && lastNamesUpdate[0].team2Name !== game.team2?.name){
-						await db('teamsNamesUpdates').insert({
-							gameId: game.id,
-							team1Name: game.team1?.name,
-							team2Name: game.team2?.name,
-							time: new Date(),
-						});
-						console.log('update names');
-					}
-				} catch (error) {console.error(error)}
 			}
 			
 			if (data.outcomes?.result){
